ee96ef2b18f83007634519dfd25a44d56c1547e8
alexv <alexv@DESKTOP-P2ERCFD>
2017-12-06 11:24:48 -0200

    Manutenções de Curso e Disciplina.

---
+++ src/java/br/edu/ifsul/controle/ControleCurso.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.controle;
+
+import br.edu.ifsul.dao.DisciplinaDAO;
+import br.edu.ifsul.dao.CursoDAO;
+import br.edu.ifsul.dao.InstituicaoDAO;
+import br.edu.ifsul.modelo.Curso;
+import br.edu.ifsul.modelo.Disciplina;
+import br.edu.ifsul.util.Util;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author alexv
+ */
+@ManagedBean(name = "controleCurso")
+@SessionScoped
+public class ControleCurso implements Serializable {
+    
+    private CursoDAO<Curso> dao;
+    private Curso objeto;
+    private DisciplinaDAO daoDisciplina;
+    private Disciplina disciplina;
+    private InstituicaoDAO daoInstituicao;
+    private Boolean novaDisciplina;
+    
+    public ControleCurso(){
+        dao = new CursoDAO<>();
+        daoDisciplina = new DisciplinaDAO();
+        daoInstituicao = new InstituicaoDAO();
+    }
+    
+    public void novaDisciplina() {
+        setDisciplina(new Disciplina());
+        setNovaDisciplina((Boolean) true);
+    }
+    
+    public void alterarDisciplina(int index){
+        setDisciplina(getObjeto().getDisciplinas().get(index));
+        novaDisciplina = false;
+    }
+    
+    public void salvarDisciplina(){
+        if (novaDisciplina){
+            getObjeto().adicionarDisciplina(getDisciplina());
+        }
+        Util.mensagemInformacao("Disciplina persistida com sucesso!");
+    }
+    
+    public void removerDisciplina(int index){
+        getObjeto().removerDisciplina(index);
+        Util.mensagemInformacao("Disciplina removida com sucesso!");
+    }
+    
+   public String listar(){
+        return "/privado/curso/listar?faces-redirect=true";
+    }
+    
+    public String novo(){
+        objeto = new Curso();
+        return "formulario?faces-redirect=true";
+    }
+    
+    public String salvar(){
+        boolean persistiu;
+        if (objeto.getId() == null){
+            persistiu = dao.persist(objeto);
+        } else {
+            persistiu = dao.merge(objeto);
+        }
+        if (persistiu){
+            Util.mensagemInformacao(dao.getMensagem());
+            return "listar?faces-redirect=true";
+        } else {
+            Util.mensagemErro(dao.getMensagem());
+            return "formulario?faces-redirect=true";
+        }
+    }
+    
+    public String cancelar(){
+        return "listar?faces-redirect=true";
+    }
+    
+    public String editar(Integer id){
+        objeto = dao.localizar(id);
+        return "formulario?faces-redirect=true";
+    }
+    
+    public void remover(Integer id){
+        objeto = dao.localizar(id);
+        if (dao.remover(objeto)){
+            Util.mensagemInformacao(dao.getMensagem());
+        } else {
+            Util.mensagemErro(dao.getMensagem());
+        }
+    }
+
+    public CursoDAO<Curso> getDao() {
+        return dao;
+    }
+
+    public void setDao(CursoDAO<Curso> dao) {
+        this.dao = dao;
+    }
+
+    public Curso getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(Curso objeto) {
+        this.objeto = objeto;
+    }
+
+    public DisciplinaDAO getDaoDisciplina() {
+        return daoDisciplina;
+    }
+
+    public void setDaoDisciplina(DisciplinaDAO daoDisciplina) {
+        this.daoDisciplina = daoDisciplina;
+    }
+
+    public InstituicaoDAO getDaoInstituicao() {
+        return daoInstituicao;
+    }
+
+    public void setDaoInstituicao(InstituicaoDAO daoInstituicao) {
+        this.daoInstituicao = daoInstituicao;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public Boolean getNovaDisciplina() {
+        return novaDisciplina;
+    }
+
+    public void setNovaDisciplina(Boolean novaDisciplina) {
+        this.novaDisciplina = novaDisciplina;
+    }
+    
+    
+    
+}
